f0a6347de27746f3b095b1f0af924e4b80442ec9
Fredrik Hedberg <fredrik.hedberg@gmail.com>
2016-11-05 11:57:49 +0100

    Split messages const and types

--- messages/types.go
+++ messages/types.go
@@ -20,17 +20,13 @@ package messages
 const (
 	SpeedMessageType            = 3
 	GLoadMessageType            = 4
+	WeatherMessageType	    = 6
 	FlightControlMessageType    = 13
+	GearsBrakesMessageType      = 14
 	PitchRollHeadingMessageType = 17
 	LatLonAltMessageType        = 20
 	EngineRPMMessageType        = 37
 	PropRPMMessageType          = 38
 	BatteryAmperageMessageType  = 53
 	BatteryVoltageMessageType   = 54
-)
-
-const (
-	feetToMeters                   = 0.3048
-	knotsToMetersPerSecond         = 0.51444444444
-	gravityInMetersPerSecondSecond = 9.80665
 )
