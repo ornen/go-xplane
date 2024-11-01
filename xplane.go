c89d60de3329d99307169ccd9f284cceba8ea53f
Florian Krick <florian.krick@node-io.dev>
2024-11-02 00:01:57 +0100

    Add message PayloadWeights

--- xplane.go
+++ xplane.go
@@ -134,6 +134,8 @@ func (x *XPlane) parse(sentence []byte) {
 		x.Messages <- messages.NewOilPressure(messageData)
 	case messages.OilTemperatureType:
 		x.Messages <- messages.NewOilTemperature(messageData)
+	case messages.PayloadWeightsType:
+		x.Messages <- messages.NewPayloadWeights(messageData)
 	case messages.SpeedMessageType:
 		x.Messages <- messages.NewSpeedMessage(messageData)
 	case messages.GLoadMessageType:
