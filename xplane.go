bbe563146b5eeec0f784f4d98b53a59f2126e996
Florian Krick <florian.krick@node-io.dev>
2024-11-01 23:38:09 +0100

    Add message EnginePower

--- xplane.go
+++ xplane.go
@@ -112,6 +112,8 @@ func (x *XPlane) parse(sentence []byte) {
 		x.Messages <- messages.NewAircraftPointWeatherTempPress(messageData)
 	case messages.AircraftRunwayConditionsType:
 		x.Messages <- messages.NewAircraftRunwayConditions(messageData)
+	case messages.EnginePowerType:
+		x.Messages <- messages.NewEnginePower(messageData)
 	case messages.SpeedMessageType:
 		x.Messages <- messages.NewSpeedMessage(messageData)
 	case messages.GLoadMessageType:
